32ea479445b6ebb18b6903642551080c9d89c7b6
david medina <davidmeddina@gmail.com>
2019-03-15 14:59:31 -0500

    Addressing h1 issue by creating views

--- solution.rb
+++ solution.rb
@@ -1,13 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  unless params[:nombre]
-    <<-HTML 
-    <h1>Hola desconocido!</h1> 
-    HTML
-  else
-    <<-HTML 
-    <h1>Hola #{params[:nombre]}!</h1> 
-    HTML
-  end
+  @name = params[:nombre]
+  erb :index  
 end
